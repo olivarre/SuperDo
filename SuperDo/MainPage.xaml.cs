af96e30fecd0eddf1ec5cf001f11c4cf19583865
Roberto Olivares <olivarre@yahoo.com>
2016-12-11 00:10:09 -0500

    Initial commit

---
+++ SuperDo/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SuperDo
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            // Handle the pan
+        }
+
+        void EditorCompleted(object sender, EventArgs e)
+        {
+            var text = ((Editor)sender).Text; // sender is cast to an Editor to enable reading the `Text` property of the view.
+        }
+
+        void EditorTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var oldText = e.OldTextValue;
+            var newText = e.NewTextValue;
+        }
+    }
+}
